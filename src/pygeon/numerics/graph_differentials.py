5e544cc30800fc47fa3ba1deadbd67eb6ddcdd92
wmboon <wietseboon@gmail.com>
2022-05-27 16:18:29 +0200

    Added graph_curl

--- src/pygeon/numerics/graph_differentials.py
+++ src/pygeon/numerics/graph_differentials.py
@@ -1,26 +1,43 @@
 import numpy as np
 import networkx as nx
+import scipy.sparse as sps
+
 
 def div(g):
-    return nx.incidence_matrix(g.graph, oriented=True)
+    div_mat = nx.incidence_matrix(g.graph, oriented=True)
+    return sps.sparse_matrix(div_mat)
+
 
 def grad(g):
-    return div(g).T
-
-def put_a_name(p, graph, line_graph, flux_label):
-    # compute the flux
-    flux = grad(line_graph)*p
-
-    attrs = {}
-    for e, f in zip(line_graph.graph.edges(), flux):
-        nodes, counts = np.unique(e, return_counts=True)
-        node = nodes[counts > 1][0]
-        if flux_label in attrs.get(node, {}):
-            attrs[node][flux_label] = np.abs(f)
-        else:
-            attrs[node] = {flux_label: np.abs(f)}
-
-    # set the attributes to the graph
-    nx.set_node_attributes(graph.graph, attrs)
+    return -div(g).T
 
 
+def curl(g):
+    cb = nx.cycle_basis(g)
+
+    incidence = np.abs(div(g))
+
+    n = np.concatenate(cb).size
+    I = np.zeros(n, dtype=int)
+    J = np.zeros(n, dtype=int)
+    V = np.zeros(n)
+
+    ind = 0
+    for (i_c, cycle) in enumerate(cb):
+        for i in np.arange(len(cycle)):
+            start = cycle[i - 1]
+            stop = cycle[i]
+
+            vec = np.zeros(g.number_of_nodes())
+            vec[start] = 1
+            vec[stop] = 1
+
+            out = incidence.T * vec
+
+            J[ind] = i_c
+            I[ind] = np.where(out == 2)[0]
+            V[ind] = np.sign(stop - start)
+
+            ind += 1
+
+    return sps.csc_matrix((V, (I, J)))
