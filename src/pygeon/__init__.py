0fa3797051f95989d98b453597e7f4df03916089
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2022-06-14 15:45:30 +0200

    update the __init__

--- src/pygeon/__init__.py
+++ src/pygeon/__init__.py
@@ -3,6 +3,7 @@ from pygeon.geometry.geometry import *
 from pygeon.numerics.differentials import grad, curl, div
 from pygeon.numerics.innerproducts import face_mass, cell_mass
 from pygeon.numerics.restrictions import remove_tip_dofs
+from pygeon.numerics.linear_system import LinearSystem
 from pygeon.rom.offline import OfflineComputations
 
 from pygeon.geometry.graph_analysis import Graph
