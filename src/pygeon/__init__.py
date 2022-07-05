b7396c17a7fcbdd4d4229477ca3ad92411d44e12
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2022-07-05 09:42:30 +0200

    remove the graph differentials, use the standard instead

--- src/pygeon/__init__.py
+++ src/pygeon/__init__.py
@@ -14,4 +14,3 @@ import pygeon.numerics.graph_innerproducts as graph_innerproducts
 
 from pygeon.filters.exporter import Exporter
 import pygeon.filters.importer as importer
-import pygeon.numerics.graph_differentials as graph_differentials
