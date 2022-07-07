86d09e77991763e842512d6fdeeec32a33a93a8d
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2022-07-07 11:32:31 +0200

    first round to fix the innerproducts.py

--- src/pygeon/numerics/innerproducts.py
+++ src/pygeon/numerics/innerproducts.py
@@ -6,64 +6,64 @@ import porepy as pp
 # ---------------------------------- Aliases ---------------------------------- #
 
 
-def cell_mass(gb, discr=None, **kwargs):
+def cell_mass(mdg, discr=None, **kwargs):
     """
     Compute the mass matrix for the piecewise constants on a grid bucket
 
     Parameters:
-        gb (pp.GridBucket).
+        mdg (pp.MixedDimensionalGrid).
         discr (pp discretization object).
 
     Returns:
         sps.csc_matrix, num_cells x num_cells
     """
 
-    return mass_matrix(gb, 0, discr, **kwargs)
+    return mass_matrix(mdg, 0, discr, **kwargs)
 
 
-def face_mass(gb, discr=pp.RT0, **kwargs):
+def face_mass(mdg, discr=pp.RT0, **kwargs):
     """
     Compute the mass matrix for discretization defined on the faces of a grid bucket
 
     Parameters:
-        gb (pp.GridBucket).
+        mdg (pp.MixedDimensionalGrid).
         discr (pp.RT0 or pp.MVEM).
 
     Returns:
         sps.csc_matrix, num_faces x num_faces
     """
 
-    return mass_matrix(gb, 1, discr, **kwargs)
+    return mass_matrix(mdg, 1, discr, **kwargs)
 
 
-def ridge_mass(gb, discr=None, **kwargs):
+def ridge_mass(mdg, discr=None, **kwargs):
     """
     Compute the mass matrix for discretization defined on the ridges of a grid bucket
 
     Parameters:
-        gb (pp.GridBucket).
+        mdg (pp.MixedDimensionalGrid).
         discr (pp discretization object).
 
     Returns:
         sps.csc_matrix, num_ridges x num_ridges
     """
 
-    return mass_matrix(gb, 2, discr, **kwargs)
+    return mass_matrix(mdg, 2, discr, **kwargs)
 
 
-def peak_mass(gb, discr=None, **kwargs):
+def peak_mass(mdg, discr=None, **kwargs):
     """
     Compute the mass matrix for discretization defined on the peaks of a grid bucket
 
     Parameters:
-        gb (pp.GridBucket).
+        mdg (pp.MixedDimensionalGrid).
         discr (pp discretization object).
 
     Returns:
         sps.csc_matrix, num_peaks x num_peaks
     """
 
-    return mass_matrix(gb, 3, discr, **kwargs)
+    return mass_matrix(mdg, 3, discr, **kwargs)
 
 
 # ---------------------------------- General ---------------------------------- #
@@ -92,12 +92,12 @@ def _g_mass_matrix(g, n_minus_k, discr=None, data=None):
         raise NotImplementedError
 
 
-def mass_matrix(gb, n_minus_k, discr, local_matrix=_g_mass_matrix, return_bmat=False):
+def mass_matrix(mdg, n_minus_k, discr, local_matrix=_g_mass_matrix, return_bmat=False):
     """
     Compute the mass matrix on a grid bucket
 
     Parameters:
-        gb (pp.GridBucket).
+        mdg (pp.MixedDimensionalGrid).
         n_minus_k (int): The difference between the dimension and the order of the differential.
         discr (pp discretization object).
         data (dict): the data object associated to the grid.
@@ -110,32 +110,32 @@ def mass_matrix(gb, n_minus_k, discr, local_matrix=_g_mass_matrix, return_bmat=F
     """
 
     bmat_g = np.empty(
-        shape=(gb.num_graph_nodes(), gb.num_graph_nodes()), dtype=sps.spmatrix
+        shape=(mdg.num_graph_nodes(), mdg.num_graph_nodes()), dtype=sps.spmatrix
     )
     bmat_mg = bmat_g.copy()
 
     # Local mass matrices
-    for g, d_g in gb:
-        nn_g = d_g["node_number"]
-        bmat_g[nn_g, nn_g] = local_matrix(g, n_minus_k, discr, d_g)
+    for sd, d_sd in mdg.subdomains(return_data=True):
+        nn_g = d_sd["node_number"]
+        bmat_g[nn_g, nn_g] = local_matrix(sd, n_minus_k, discr, d_sd)
         bmat_mg[nn_g, nn_g] = sps.csc_matrix(bmat_g[nn_g, nn_g].shape)
 
     # Mortar contribution
     if n_minus_k == 1:
-        for e, d_e in gb.edges():
+        #for e, d_e in mdg.interfaces(return_data=True):
+        for intf, d_e in mdg.interfaces(return_data=True):
             # Get adjacent grids and mortar_grid
-            g_up = gb.nodes_of_edge(e)[1]
-            mg = d_e["mortar_grid"]
+            pair = mdg.interface_to_subdomain_pair(intf)
 
             # Get indices in grid_bucket
-            nn_g = gb.node_props(g_up, "node_number")
+            nn_g = mdg.subdomain_data(pair[0])["node_number"]
 
             # Local mortar mass matrix
-            kn = d_e["parameters"]["flow"]["normal_diffusivity"]
+            kn = d_intf["parameters"]["flow"]["normal_diffusivity"]
             bmat_mg[nn_g, nn_g] += (
-                mg.signed_mortar_to_primary
-                * sps.diags(1.0 / mg.cell_volumes / kn)
-                * mg.signed_mortar_to_primary.T
+                intf.signed_mortar_to_primary
+                * sps.diags(1.0 / intf.cell_volumes / kn)
+                * intf.signed_mortar_to_primary.T
             )
 
     if return_bmat:
@@ -147,12 +147,12 @@ def mass_matrix(gb, n_minus_k, discr, local_matrix=_g_mass_matrix, return_bmat=F
 # ---------------------------------- Lumped ---------------------------------- #
 
 
-def lumped_mass_matrix(gb, n_minus_k, discr):
+def lumped_mass_matrix(mdg, n_minus_k, discr):
     """
     Compute the mass-lumped mass matrix on a grid bucket
 
     Parameters:
-        gb (pp.GridBucket).
+        mdg (pp.MixedDimensionalGrid).
         n_minus_k (int): The difference between the dimension and the order of the differential.
         discr (pp discretization object).
 
@@ -160,7 +160,7 @@ def lumped_mass_matrix(gb, n_minus_k, discr):
         sps.csc_matrix, num_dofs x num_dofs
     """
 
-    return mass_matrix(gb, n_minus_k, discr, local_matrix=_g_lumped_mass)
+    return mass_matrix(mdg, n_minus_k, discr, local_matrix=_g_lumped_mass)
 
 
 def _g_lumped_mass(g, n_minus_k, discr, data):
@@ -169,7 +169,7 @@ def _g_lumped_mass(g, n_minus_k, discr, data):
     For k = 1, this is the matrix that leads to a TPFA discretization.
 
     Parameters:
-        gb (pp.GridBucket).
+        mdg (pp.MixedDimensionalGrid).
         n_minus_k (int): The difference between the dimension and the order of the differential.
         discr (pp discretization object).
         data (dict): the data object associated to the grid.
