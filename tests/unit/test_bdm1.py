43c4763b984f39cd912e7dd2c111cf342bc8efe9
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2022-09-16 14:23:51 +0200

    more tests for bdm1

--- tests/unit/test_bdm1.py
+++ tests/unit/test_bdm1.py
@@ -27,8 +27,6 @@ class BDM1Test(unittest.TestCase):
 
         check = E * mass_bdm1 * E.T - mass_rt0
 
-        import pdb; pdb.set_trace()
-
     def test1(self):
         N, dim = 2, 3
         sd = pp.StructuredTetrahedralGrid([N] * dim, [1] * dim)
@@ -49,8 +47,86 @@ class BDM1Test(unittest.TestCase):
 
         import pdb; pdb.set_trace()
 
+    def test3(self):
+        N, dim = 20, 2
+        sd = pp.StructuredTriangleGrid([N] * dim, [1] * dim)
+        pg.convert_from_pp(sd)
+        sd.compute_geometry()
+
+        discr_bdm1 = pg.BDM1("flow")
+        mass_bdm1 = discr_bdm1.assemble_lumped_matrix(sd, None)
+
+        div = discr_bdm1.assemble_div(sd)
+
+        # assemble the saddle point problem
+        spp = sps.bmat([[mass_bdm1, -div.T],
+                        [      div,  None]], format="csc")
+
+        b_faces = sd.tags["domain_boundary_faces"].nonzero()[0]
+
+        b_face_centers = sd.face_centers[:, b_faces]
+
+        faces, _, sign = sps.find(sd.cell_faces)
+        sign = sign[np.unique(faces, return_index=True)[1]]
+
+        bc_val = np.zeros(sd.num_faces*2)
+        bc_val[b_faces] = -sign[b_faces] * b_face_centers[0, :] / 2
+        bc_val[b_faces+sd.num_faces] = -sign[b_faces] * b_face_centers[0, :] / 2
+
+        rhs = np.zeros(spp.shape[0])
+        rhs[:bc_val.size] += bc_val
+
+        # solve the problem
+        ls = pg.LinearSystem(spp, rhs)
+        x = ls.solve()
+
+        q = x[:bc_val.size]
+        p = x[-sd.num_cells:]
+
+        save = pp.Exporter(sd, "sol")
+        save.write_vtu([("p", p)])
+
+    def test4(self):
+        N, dim = 3, 3
+        sd = pp.StructuredTetrahedralGrid([N] * dim, [1] * dim)
+        pg.convert_from_pp(sd)
+        sd.compute_geometry()
+
+        discr_bdm1 = pg.BDM1("flow")
+        mass_bdm1 = discr_bdm1.assemble_lumped_matrix(sd, None)
+
+        div = discr_bdm1.assemble_div(sd)
+
+        # assemble the saddle point problem
+        spp = sps.bmat([[mass_bdm1, -div.T],
+                        [      div,  None]], format="csc")
+
+        b_faces = sd.tags["domain_boundary_faces"].nonzero()[0]
+
+        b_face_centers = sd.face_centers[:, b_faces]
+
+        faces, _, sign = sps.find(sd.cell_faces)
+        sign = sign[np.unique(faces, return_index=True)[1]]
+
+        bc_val = np.zeros(sd.num_faces*sd.dim)
+        bc_val[b_faces] = -sign[b_faces] * b_face_centers[0, :] / sd.dim
+        bc_val[b_faces+sd.num_faces] = -sign[b_faces] * b_face_centers[0, :] / sd.dim
+        bc_val[b_faces+2*sd.num_faces] = -sign[b_faces] * b_face_centers[0, :] / sd.dim
+
+        rhs = np.zeros(spp.shape[0])
+        rhs[:bc_val.size] += bc_val
+
+        # solve the problem
+        ls = pg.LinearSystem(spp, rhs)
+        x = ls.solve()
+
+        q = x[:bc_val.size]
+        p = x[-sd.num_cells:]
+
+        save = pp.Exporter(sd, "sol")
+        save.write_vtu([("p", p)])
 
 if __name__ == "__main__":
-    BDM1Test().test1()
+    BDM1Test().test4()
     #unittest.main()
 
