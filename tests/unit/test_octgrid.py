9fe257284a9855749c6e53906bae00d6400a02cb
wmboon <wietseboon@gmail.com>
2023-01-17 12:07:11 +0100

    Started more involved quadrature

--- tests/unit/test_octgrid.py
+++ tests/unit/test_octgrid.py
@@ -7,13 +7,21 @@ import scipy.sparse as sps
 
 
 class OctGridTest(unittest.TestCase):
-    def test_simple_case(self):
-        nx = [5, 5]
+    def test_single_octagon(self):
+        nx = [1, 1]
         sd = pg.OctGrid(nx)
         sd.compute_geometry()
 
-        pp.plot_grid(sd, info="all", alpha=0)
+        face_areas_known = np.full_like(sd.face_areas, np.sqrt(2) - 1)
+        face_areas_known[8:] = 1 / (2 + np.sqrt(2))
+
+        self.assertTrue(np.allclose(sd.face_areas, face_areas_known))
+
+        cell_volumes_known = np.full_like(sd.cell_volumes, 0.75 - 1 / np.sqrt(2))
+        cell_volumes_known[0] = 2 * np.sqrt(2) - 2
+
+        self.assertTrue(np.allclose(sd.cell_volumes, cell_volumes_known))
 
 
 if __name__ == "__main__":
-    OctGridTest().test_simple_case()
+    unittest.main()
