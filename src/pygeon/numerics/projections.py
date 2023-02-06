9cac6fef445790e8e82f3a48240d53c12b8bc8e5
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2023-02-06 08:52:29 +0100

    allow the projection operators to have the choice to convert the matrix from block to full-sparse

--- src/pygeon/numerics/projections.py
+++ src/pygeon/numerics/projections.py
@@ -4,6 +4,21 @@ import scipy.sparse as sps
 
 
 def eval_at_cell_centers(mdg, discr=None, **kwargs):
+    """
+    Create an operator that evalates a solution in the cell centers.
+
+    Parameters:
+        mdg (pp.MixedDimensionalGrid): The mixed dimensional grid.
+        discr: The discretization used for th evaluation, if not provided a standard discretization is used. Default to
+        None.
+        kwargs: Optional parameters
+            to_sparse: Convert to a sparse matrix from sparse sub-blocks. Default True.
+            sps_format: Format of the sparse matrix when converted. Delfault csc.
+    """
+
+    to_sparse = kwargs.get("to_sparse", True)
+    sps_format = kwargs.get("sps_format", "csc")
+
     bmat_sd = np.empty(
         shape=(mdg.num_subdomains(), mdg.num_subdomains()), dtype=sps.spmatrix
     )
@@ -12,13 +27,26 @@ def eval_at_cell_centers(mdg, discr=None, **kwargs):
     for (nn_sd, sd) in enumerate(mdg.subdomains()):
         bmat_sd[nn_sd, nn_sd] = discr.eval_at_cell_centers(sd)
 
-    return sps.bmat(bmat_sd, format="csc")
+    pg.bmat.replace_nones_with_zeros(bmat_sd)
+    return sps.bmat(bmat_sd, format=sps_format) if to_sparse else bmat_sd
 
 
 def proj_faces_to_cells(mdg, discr=None, **kwargs):
     """
-    Computes the block matrices of the mass matrix
+    Create an operator that evalates a solution defined on the faces in the cell centers.
+
+    Parameters:
+        mdg (pp.MixedDimensionalGrid): The mixed dimensional grid.
+        discr: The discretization used for th evaluation, if not provided a standard discretization is used. Default to
+        None.
+        kwargs: Optional parameters
+            to_sparse: Convert to a sparse matrix from sparse sub-blocks. Default True.
+            sps_format: Format of the sparse matrix when converted. Delfault csc.
     """
+
+    to_sparse = kwargs.get("to_sparse", True)
+    sps_format = kwargs.get("sps_format", "csc")
+
     bmat_sd = np.empty(
         shape=(mdg.num_subdomains(), mdg.num_subdomains()), dtype=sps.spmatrix
     )
@@ -33,4 +61,5 @@ def proj_faces_to_cells(mdg, discr=None, **kwargs):
             "vector_proj"
         ]
 
-    return sps.bmat(bmat_sd, format="csc")
+    pg.bmat.replace_nones_with_zeros(bmat_sd)
+    return sps.bmat(bmat_sd, format=sps_format) if to_sparse else bmat_sd
