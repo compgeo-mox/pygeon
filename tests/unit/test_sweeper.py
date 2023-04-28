1ecbdfb7ed9ed0dd6211e952265184e987859104
wmboon <wietseboon@gmail.com>
2023-04-28 11:25:57 +0200

    testing for pressure reconstruction

--- tests/unit/test_sweeper.py
+++ tests/unit/test_sweeper.py
@@ -3,42 +3,62 @@
 import unittest
 import numpy as np
 import porepy as pp
+import scipy.sparse as sps
+
 import pygeon as pg
 
 
 class SweeperUnitTest(unittest.TestCase):
-    def check(self, sd):
+    def check_flux(self, sd):
+        """
+        Check whether the constructed flux balances the given mass-source
+        """
         swp = pg.Sweeper(sd)
-        f = np.arange(sd.num_cells)
+        f = np.arange(swp.expand.shape[1])
         q_f = swp.sweep(f)
 
         self.assertTrue(np.allclose(pg.cell_mass(sd) @ pg.div(sd) @ q_f, f))
 
-    def check_mdg(self, mdg):
-        swp = pg.Sweeper(mdg)
-        f = np.arange(mdg.num_subdomain_cells())
-        q_f = swp.sweep(f)
+    def check_pressure(self, sd):
+        """
+        Check whether the post-processing of the pressure is correct
+        """
+        div = pg.cell_mass(sd) @ pg.div(sd)
+        face_mass = pg.face_mass(sd)
+        system = sps.bmat([[face_mass, -div.T], [div, None]], "csc")
 
-        self.assertTrue(np.allclose(pg.cell_mass(mdg) @ pg.div(mdg) @ q_f, f))
+        f = np.ones(div.shape[0])
+        rhs = np.hstack([np.zeros(div.shape[1]), f])
+
+        x = sps.linalg.spsolve(system, rhs)
+        q = x[: div.shape[1]]
+        p = x[div.shape[1] :]
+
+        swp = pg.Sweeper(sd)
+        p_swp = swp.sweep_transpose(face_mass @ q)
+
+        self.assertTrue(np.allclose(p, p_swp))
 
     def test_cart_grid(self):
         N = 3
         for dim in np.arange(1, 4):
             sd = pp.CartGrid([N] * dim, [1] * dim)
             pg.convert_from_pp(sd)
-            self.check(sd)
+            self.check_flux(sd)
 
     def test_structured_triangle(self):
         N, dim = 3, 2
         sd = pp.StructuredTriangleGrid([N] * dim, [1] * dim)
         pg.convert_from_pp(sd)
-        self.check(sd)
+        self.check_flux(sd)
+        self.check_pressure(sd)
 
     def test_structured_tetra(self):
         N, dim = 3, 3
         sd = pp.StructuredTetrahedralGrid([N] * dim, [1] * dim)
         pg.convert_from_pp(sd)
-        self.check(sd)
+        self.check_flux(sd)
+        self.check_pressure(sd)
 
     def test_2d_mdg(self):
         grids = [
@@ -51,13 +71,15 @@ class SweeperUnitTest(unittest.TestCase):
             mdg, _ = g()
             pg.convert_from_pp(mdg)
             mdg.compute_geometry()
-            self.check_mdg(mdg)
+            self.check_flux(mdg)
+            self.check_pressure(mdg)
 
     def test_3d_mdg(self):
         mdg, _ = pp.md_grids_3d.single_horizontal()
         pg.convert_from_pp(mdg)
         mdg.compute_geometry()
-        self.check_mdg(mdg)
+        self.check_flux(mdg)
+        self.check_pressure(mdg)
 
 
 if __name__ == "__main__":
