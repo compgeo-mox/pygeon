77de4e827200d69ccbd8f16f0866d0c2ab8af299
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2023-05-05 08:21:15 +0200

    add test for a unit cube

--- tests/unit/test_create_grid.py
+++ tests/unit/test_create_grid.py
@@ -16,6 +16,14 @@ class StandardGridTest(unittest.TestCase):
         self.assertTrue(np.isclose(sd.num_faces, 25))
         self.assertTrue(np.isclose(sd.num_nodes, 12))
 
+    def test_unit_cube(self):
+        mesh_size = 0.5
+        sd = pg.grid_unitary(3, mesh_size, as_mdg=False)
+
+        self.assertTrue(np.isclose(sd.num_cells, 100))
+        self.assertTrue(np.isclose(sd.num_faces, 242))
+        self.assertTrue(np.isclose(sd.num_nodes, 45))
+
 
 if __name__ == "__main__":
     unittest.main()
