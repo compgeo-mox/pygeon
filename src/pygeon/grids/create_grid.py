ae00ff8eca72f89af849fd79aead7385a605b5b7
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2023-05-12 14:33:11 +0200

    add the possibility to create from a general domain

--- src/pygeon/grids/create_grid.py
+++ src/pygeon/grids/create_grid.py
@@ -3,6 +3,31 @@ import porepy as pp
 import pygeon as pg
 
 
+def grid_from_domain(domain, mesh_size, **kwargs):
+    """
+    Create a grid from a domain with a mesh size.
+
+    Parameters:
+        domain (pp.Domain): the domain of the grid
+        mesh_size (double): the mesh size
+        kwargs: additional options are the following
+            mesh_size_min (double): the minimum mesh size, default is mesh_size
+            as_mdg (bool): return the grid as a mixed-dimensional grid
+
+        Returns:
+            Either a pg.MixedDimensionalGrid or a pg.Grid.
+    """
+    mesh_size_min = kwargs.get("mesh_size_min", mesh_size / 10)
+    mesh_kwargs = {"mesh_size_frac": mesh_size, "mesh_size_min": mesh_size_min}
+    mdg = pp.create_fracture_network(domain=domain).mesh(mesh_kwargs)
+
+    pg.convert_from_pp(mdg)
+    if kwargs.get("as_mdg", True):
+        return mdg
+    else:
+        return mdg.subdomains(dim=mdg.dim_max())[0]
+
+
 def unit_grid(dim, mesh_size, **kwargs):
     """
     Create a unit square or cube grid with a mesh size.
@@ -23,13 +48,4 @@ def unit_grid(dim, mesh_size, **kwargs):
         bbox.update({"zmin": 0, "zmax": 1})
 
     domain = pp.Domain(bounding_box=bbox)
-
-    mesh_size_min = kwargs.get("mesh_size_min", mesh_size / 10)
-    mesh_kwargs = {"mesh_size_frac": mesh_size, "mesh_size_min": mesh_size_min}
-    mdg = pp.create_fracture_network(domain=domain).mesh(mesh_kwargs)
-
-    pg.convert_from_pp(mdg)
-    if kwargs.get("as_mdg", True):
-        return mdg
-    else:
-        return mdg.subdomains(dim=dim)[0]
+    return grid_from_domain(domain, mesh_size, **kwargs)
--- tests/unit/test_create_grid.py
+++ tests/unit/test_create_grid.py
@@ -4,6 +4,7 @@ Module contains a unit test for the standard grid computation.
 import unittest
 import numpy as np
 
+import porepy as pp
 import pygeon as pg
 
 
@@ -23,6 +24,36 @@ class StandardGridTest(unittest.TestCase):
         self.assertTrue(np.isclose(sd.num_cells, 100))
         self.assertTrue(np.isclose(sd.num_faces, 242))
         self.assertTrue(np.isclose(sd.num_nodes, 45))
+
+    def test_triangle(self):
+        line_1 = np.array([[0, 1], [0, 0]])
+        line_2 = np.array([[1, 0], [0, 1]])
+        line_3 = np.array([[0, 0], [1, 0]])
+        lines = [line_1, line_2, line_3]
+        domain = pp.Domain(polytope=lines)
+
+        mesh_size = 0.5
+        sd = pg.grid_from_domain(domain, mesh_size, as_mdg=False)
+
+        self.assertTrue(np.isclose(sd.num_cells, 7))
+        self.assertTrue(np.isclose(sd.num_faces, 14))
+        self.assertTrue(np.isclose(sd.num_nodes, 8))
+
+    def test_concave_pentagon(self):
+        line_1 = np.array([[0, 1], [0, 0]])
+        line_2 = np.array([[1, 1], [0, 1]])
+        line_3 = np.array([[1, 0.5], [1, 0.5]])
+        line_4 = np.array([[0.5, 0], [0.5, 1]])
+        line_5 = np.array([[0, 0], [1, 0]])
+        lines = [line_1, line_2, line_3, line_4, line_5]
+        domain = pp.Domain(polytope=lines)
+
+        mesh_size = 0.5
+        sd = pg.grid_from_domain(domain, mesh_size, as_mdg=False)
+
+        self.assertTrue(np.isclose(sd.num_cells, 14))
+        self.assertTrue(np.isclose(sd.num_faces, 26))
+        self.assertTrue(np.isclose(sd.num_nodes, 13))
 
 
 if __name__ == "__main__":
