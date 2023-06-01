820e9d905b2e0747d2d1eda305ad61b201e0d2ed
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2023-06-01 13:33:59 +0100

    Update create grid (#23)
    * drop dependency from python 3.8 and 3.9. add dependency from python 3.11
    
    * update packages version dependency
    
    * update the creation of a simple unit square and cube
    
    * update the init
    
    * flake8
    
    * removed pentagon grid
    updated unit square grid
    
    * change default keyword from "flow" to "default"
    
    * update comments for the grid creation
    
    * add test for a unit cube
    
    * remove useless file
    
    * Delete forchheimer.ipynb
    
    * fix black for converter.py
    
    * Delete converter.py
    
    * Delete wave.ipynb
    
    * change the name of unit grid
    
    * update gitignore
    
    * add the possibility to create from a general domain
    
    * add the possibility to create a grid from a set of boundary points
    
    * update the tests to make them more robust with respect to the gmsh version
    
    * apply isort to create_grid.py
    
    * fixed isort
    
    ---------
    
    Co-authored-by: wmboon <wietseboon@gmail.com>

--- src/pygeon/grids/create_grid.py
+++ src/pygeon/grids/create_grid.py
@@ -1,6 +1,58 @@
+import numpy as np
 import porepy as pp
 
 import pygeon as pg
+
+
+def grid_from_domain(domain, mesh_size, **kwargs):
+    """
+    Create a grid from a domain with a mesh size.
+
+    Parameters:
+        domain (pp.Domain): the domain of the grid
+        mesh_size (double): the mesh size
+        kwargs: additional options are the following
+            mesh_size_min (double): the minimum mesh size, default is mesh_size
+            as_mdg (bool): return the grid as a mixed-dimensional grid
+
+        Returns:
+            Either a pg.MixedDimensionalGrid or a pg.Grid.
+    """
+    mesh_size_min = kwargs.get("mesh_size_min", mesh_size / 10)
+    mesh_kwargs = {"mesh_size_frac": mesh_size, "mesh_size_min": mesh_size_min}
+    mdg = pp.create_fracture_network(domain=domain).mesh(mesh_kwargs)
+
+    pg.convert_from_pp(mdg)
+    if kwargs.get("as_mdg", True):
+        return mdg
+    else:
+        return mdg.subdomains(dim=mdg.dim_max())[0]
+
+
+def grid_from_boundary_pts(pts, mesh_size, **kwargs):
+    """
+    Create a 2d grid from a set of nodes, portions the boundary of the grid
+    are constructed from subsequent nodes, with a mesh size.
+
+    Parameters:
+        pts (np.ndarray): the ordered points representing the boundary
+        mesh_size (double): the mesh size
+        kwargs: additional options are the following
+            mesh_size_min (double): the minimum mesh size, default is mesh_size
+            as_mdg (bool): return the grid as a mixed-dimensional grid
+
+        Returns:
+            Either a pg.MixedDimensionalGrid or a pg.Grid.
+    """
+    # Create the lines representing the boundary of the domain
+    idx = np.arange(pts.shape[1])
+    order = np.vstack((idx, np.roll(idx, -1))).flatten(order="F")
+    lines = np.split(pts[:, order], pts.shape[1], axis=1)
+
+    # Create the domain
+    domain = pp.Domain(polytope=lines)
+
+    return grid_from_domain(domain, mesh_size, **kwargs)
 
 
 def unit_grid(dim, mesh_size, **kwargs):
@@ -23,13 +75,4 @@ def unit_grid(dim, mesh_size, **kwargs):
         bbox.update({"zmin": 0, "zmax": 1})
 
     domain = pp.Domain(bounding_box=bbox)
-
-    mesh_size_min = kwargs.get("mesh_size_min", mesh_size / 10)
-    mesh_kwargs = {"mesh_size_frac": mesh_size, "mesh_size_min": mesh_size_min}
-    mdg = pp.create_fracture_network(domain=domain).mesh(mesh_kwargs)
-
-    pg.convert_from_pp(mdg)
-    if kwargs.get("as_mdg", True):
-        return mdg
-    else:
-        return mdg.subdomains(dim=dim)[0]
+    return grid_from_domain(domain, mesh_size, **kwargs)
--- tests/unit/test_create_grid.py
+++ tests/unit/test_create_grid.py
@@ -4,17 +4,116 @@ Module contains a unit test for the standard grid computation.
 import unittest
 import numpy as np
 
+import porepy as pp
 import pygeon as pg
 
 
 class StandardGridTest(unittest.TestCase):
+    def check_grid(self, sd, dim, boundary):
+        self.assertTrue(sd.dim == dim)
+
+        # check if the boundary faces belong to the imposed boundary
+        faces = sd.tags["domain_boundary_faces"]
+        bf = sd.face_centers[:, faces]
+
+        # check if the boundary nodes belong to the imposed boundary
+        nodes = sd.tags["domain_boundary_nodes"]
+        bn = sd.nodes[:, nodes]
+
+        face_on_boundary = np.zeros(faces.sum(), dtype=bool)
+        node_on_boundary = np.zeros(nodes.sum(), dtype=bool)
+        for side in boundary:
+            if dim == 2:
+                dist_faces, _ = pp.geometry.distances.points_segments(
+                    bf[:2, :], side[:, 0], side[:, 1]
+                )
+                dist_nodes, _ = pp.geometry.distances.points_segments(
+                    bn[:2, :], side[:, 0], side[:, 1]
+                )
+            else:
+                dist_faces, _, _ = pp.geometry.distances.points_polygon(bf, side)
+                dist_nodes, _, _ = pp.geometry.distances.points_polygon(bn, side)
+
+            face_on_boundary[np.isclose(dist_faces, 0).ravel()] = True
+            node_on_boundary[np.isclose(dist_nodes, 0).ravel()] = True
+
+        self.assertTrue(np.all(face_on_boundary))
+        self.assertTrue(np.all(node_on_boundary))
+
     def test_unit_square(self):
         mesh_size = 0.5
         sd = pg.unit_grid(2, mesh_size, as_mdg=False)
 
-        self.assertTrue(np.isclose(sd.num_cells, 14))
-        self.assertTrue(np.isclose(sd.num_faces, 25))
-        self.assertTrue(np.isclose(sd.num_nodes, 12))
+        side_1 = np.array([[0, 1], [0, 0]])
+        side_2 = np.array([[1, 1], [0, 1]])
+        side_3 = np.array([[1, 0], [1, 1]])
+        side_4 = np.array([[0, 0], [1, 0]])
+        boundary = [side_1, side_2, side_3, side_4]
+        self.check_grid(sd, 2, boundary)
+
+    def test_unit_cube(self):
+        mesh_size = 0.5
+        sd = pg.unit_grid(3, mesh_size, as_mdg=False)
+
+        side_1 = np.array([[0, 1, 1, 0], [0, 0, 1, 1], [0, 0, 0, 0]])
+        side_2 = np.array([[1, 1, 1, 1], [0, 1, 1, 0], [0, 0, 1, 1]])
+        side_3 = np.array([[0, 1, 1, 0], [0, 0, 0, 0], [0, 0, 1, 1]])
+        side_4 = np.array([[0, 0, 0, 0], [0, 1, 1, 0], [0, 0, 1, 1]])
+        side_5 = np.array([[0, 1, 1, 0], [1, 1, 1, 1], [0, 0, 1, 1]])
+        side_6 = np.array([[0, 1, 1, 0], [0, 0, 1, 1], [1, 1, 1, 1]])
+        boundary = [side_1, side_2, side_3, side_4, side_5, side_6]
+        self.check_grid(sd, 3, boundary)
+
+    def test_triangle(self):
+        side_1 = np.array([[0, 1], [0, 0]])
+        side_2 = np.array([[1, 0], [0, 1]])
+        side_3 = np.array([[0, 0], [1, 0]])
+        boundary = [side_1, side_2, side_3]
+        domain = pp.Domain(polytope=boundary)
+
+        mesh_size = 0.5
+        sd = pg.grid_from_domain(domain, mesh_size, as_mdg=False)
+
+        self.check_grid(sd, 2, boundary)
+
+    def test_concave_pentagon(self):
+        side_1 = np.array([[0, 1], [0, 0]])
+        side_2 = np.array([[1, 1], [0, 1]])
+        side_3 = np.array([[1, 0.5], [1, 0.5]])
+        side_4 = np.array([[0.5, 0], [0.5, 1]])
+        side_5 = np.array([[0, 0], [1, 0]])
+        boundary = [side_1, side_2, side_3, side_4, side_5]
+        domain = pp.Domain(polytope=boundary)
+
+        mesh_size = 0.5
+        sd = pg.grid_from_domain(domain, mesh_size, as_mdg=False)
+
+        self.check_grid(sd, 2, boundary)
+
+    def test_triangle_from_pts(self):
+        pts = np.array([[0, 1, 0], [0, 0, 1]])
+
+        mesh_size = 0.5
+        sd = pg.grid_from_boundary_pts(pts, mesh_size, as_mdg=False)
+
+        side_1 = np.array([[0, 1], [0, 0]])
+        side_2 = np.array([[1, 0], [0, 1]])
+        side_3 = np.array([[0, 0], [1, 0]])
+        boundary = [side_1, side_2, side_3]
+        self.check_grid(sd, 2, boundary)
+
+    def test_concave_pentagon_from_pts(self):
+        pts = np.array([[0, 1, 1, 0.5, 0], [0, 0, 1, 0.5, 1]])
+        mesh_size = 0.5
+        sd = pg.grid_from_boundary_pts(pts, mesh_size, as_mdg=False)
+
+        side_1 = np.array([[0, 1], [0, 0]])
+        side_2 = np.array([[1, 1], [0, 1]])
+        side_3 = np.array([[1, 0.5], [1, 0.5]])
+        side_4 = np.array([[0.5, 0], [0.5, 1]])
+        side_5 = np.array([[0, 0], [1, 0]])
+        boundary = [side_1, side_2, side_3, side_4, side_5]
+        self.check_grid(sd, 2, boundary)
 
     def test_unit_cube(self):
         mesh_size = 0.5
