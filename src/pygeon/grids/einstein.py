0399e93cf7c41ff2b15ac6a8d9ec1b0bc654e1c5
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2023-11-24 18:10:17 +0100

    add docs einstein

--- src/pygeon/grids/einstein.py
+++ src/pygeon/grids/einstein.py
@@ -1,3 +1,4 @@
+from typing import Union
 import xml.etree.ElementTree as ET
 
 import numpy as np
@@ -12,7 +13,16 @@ class EinSteinGrid(pg.Grid):
     https://cs.uwaterloo.ca/~csk/hat/app.html
     """
 
-    def __init__(self, file_name):
+    def __init__(self, file_name: str) -> None:
+        """
+        Initialize the EinSteinGrid object.
+
+        Args:
+            file_name (str): The name of the file containing the grid data.
+
+        Returns:
+            None
+        """
         # read all the data from the file
         self.poly, self.trans, self.root = self.from_file(file_name)
         # adding the hanging nodes to fix T-junctions
@@ -30,15 +40,36 @@ class EinSteinGrid(pg.Grid):
             2, coords, face_nodes, cell_faces, "EinSteinGrid"
         )
 
-    def add_hanging_node(self):
-        """Function to add the hanging nodes for each polygons to fix
+    def add_hanging_node(self) -> None:
+        """
+        Function to add the hanging nodes for each polygons to fix
         T-junctions.
+
+        This function adds a hanging node to each polygon in the grid to fix T-junctions.
+        The hanging node is inserted at the midpoint of the third and fourth vertices of each polygon.
+
+        Args:
+            None
+
+        Returns:
+            None
         """
         for key, p in self.poly.items():
             self.poly[key] = np.insert(p, 3, 0.5 * (p[:, 2] + p[:, 3]), axis=1)
 
-    def build_connectivity(self):
-        """Build the connectivity of the grid."""
+    def build_connectivity(self) -> Union[np.ndarray, sps.csc_matrix, sps.csc_matrix]:
+        """
+        Build the connectivity of the grid.
+
+        Args:
+            None
+
+        Returns:
+            Tuple[np.ndarray, sps.csc_matrix, sps.csc_matrix]: A tuple containing the following:
+                - coords (np.ndarray): The rescaled points of the polygons in the unit square.
+                - cell_faces (sps.csc_matrix): The sparse matrix representing the cell-face relations.
+                - face_nodes (sps.csc_matrix): The sparse matrix representing the face-nodes relations.
+        """
         # rescale the points of the polygons to be in the unit square
         all_pts = self.rescale()
 
@@ -79,17 +110,30 @@ class EinSteinGrid(pg.Grid):
 
         return coords, cell_faces, face_nodes
 
-    def rescale(self):
-        """Rescale the coordinates of the polygons in the unit square."""
+    def rescale(self) -> np.ndarray:
+        """
+        Rescale the coordinates of the polygons in the unit square.
+
+        Returns:
+            np.ndarray: The rescaled coordinates of the polygons.
+        """
         all_pts = np.hstack(self.poly_list)
         all_pts[2] = 0
         all_pts -= np.atleast_2d(np.min(all_pts, axis=1)).T
         all_pts /= np.max(all_pts)
         return all_pts
 
-    def poly_adder(self, input_str, transform):
-        """Recursive function to build all the polygons based on the transformation
-        matrices."""
+    def poly_adder(self, input_str: str, transform: np.ndarray) -> None:
+        """
+        Recursive function to build all the polygons based on the transformation matrices.
+
+        Args:
+            input_str (str): The input string representing the current tag.
+            transform (np.ndarray): The transformation matrix.
+
+        Returns:
+            None
+        """
         if input_str in self.poly:
             # if the current tag is a polygon then build it and add it to the list
             self.poly_list.append(transform @ self.poly[input_str])
@@ -99,8 +143,19 @@ class EinSteinGrid(pg.Grid):
             for sub in self.trans[input_str]:
                 self.poly_adder(sub[0], transform @ sub[1])
 
-    def from_file(self, file_name):
-        """Read svg file and create the first data structure."""
+    def from_file(self, file_name: str) -> Union[dict, dict, tuple]:
+        """
+        Read an SVG file and create the first data structure.
+
+        Args:
+            file_name (str): The path to the SVG file.
+
+        Returns:
+            Union[dict, dict, tuple]: A tuple containing three elements:
+                - poly_dict: A dictionary mapping polygon IDs to their corresponding polygons.
+                - trans_dict: A dictionary mapping transformation IDs to a list of transformations.
+                - use_info: A tuple containing the ID and transformation matrix of the root use element.
+        """
         root = ET.parse(file_name).getroot()[0]
         tag_str = r"{http://www.w3.org/1999/xlink}href"
 
@@ -131,15 +186,31 @@ class EinSteinGrid(pg.Grid):
 
         return poly_dict, trans_dict, (use_id, use_matrix)
 
-    def as_polygon(self, pts):
-        """Convert a string to a 2d polygon in homogeneous coordinate"""
+    def as_polygon(self, pts: str) -> np.ndarray:
+        """
+        Convert a string to a 2d polygon in homogeneous coordinate
+
+        Args:
+            pts (str): The string representing the polygon points
+
+        Returns:
+            np.ndarray: The 2d polygon in homogeneous coordinate
+        """
         pts = np.fromstring(pts.replace(",", " "), sep=" ")
         pts = pts.reshape((2, -1), order="F")
         # add the homogeneous coordinate and return the pts
         return np.vstack((pts, np.ones(pts.shape[1])))
 
-    def as_matrix(self, mat):
-        """Convert a string to a 2d matrix in homogeneous coordinate"""
+    def as_matrix(self, mat: str) -> np.ndarray:
+        """
+        Convert a string to a 2d matrix in homogeneous coordinate
+
+        Args:
+            mat (str): The string representation of the matrix
+
+        Returns:
+            np.ndarray: The 2d matrix in homogeneous coordinate
+        """
         mat = np.fromstring(mat[7:-1], sep=" ")
         mat = mat.reshape((2, -1), order="F")
         # add the homogeneous coordinate and return the matrix
