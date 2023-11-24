f5643d2df101d908d9f6d9cf3cd7ed39413b5329
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2023-11-24 17:37:14 +0100

    update docstring for the mdg

--- src/pygeon/grids/md_grid.py
+++ src/pygeon/grids/md_grid.py
@@ -7,11 +7,21 @@ class MixedDimensionalGrid(pp.MixedDimensionalGrid):
         super(MixedDimensionalGrid, self).__init__(*args, **kwargs)
         self.initialize_data()
 
-    def compute_geometry(self):
+    def compute_geometry(self) -> None:
         """
         Compute geometric entities and tags for the subdomains and interfaces.
-        """
 
+        This method iterates over the subdomains and interfaces of the grid
+        and computes their geometric entities and tags. It calls the
+        `compute_geometry` method of each subdomain and interface, passing
+        the necessary parameters. Finally, it tags the leaf nodes of the grid.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
         for sd in self.subdomains():
             sd.compute_geometry()
 
@@ -22,6 +32,18 @@ class MixedDimensionalGrid(pp.MixedDimensionalGrid):
         self.tag_leafs()
 
     def initialize_data(self) -> None:
+        """
+        Initializes the data for the multi-dimensional grid.
+
+        This method initializes the data for each subdomain and interface in the multi-dimensional grid.
+        It sets the parameters and discretization matrices for each subdomain and interface.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
         for sd, data in self.subdomains(return_data=True):
             perm = pp.SecondOrderTensor(np.ones(sd.num_cells))
             data[pp.PARAMETERS] = pp.Parameters(
@@ -44,14 +66,15 @@ class MixedDimensionalGrid(pp.MixedDimensionalGrid):
             data[pp.DISCRETIZATION_MATRICES] = {}
 
     def num_subdomain_faces(self, cond=None) -> int:
-        """Compute the total number of faces of the mixed-dimensional grid.
+        """
+        Compute the total number of faces of the mixed-dimensional grid.
 
         A function can be passed to filter subdomains and/or interfaces.
 
         Args:
             cond: optional, predicate with a grid as input.
 
-        Return:
+        Returns:
             int: the total number of faces of the mixed-dimensional grid.
 
         """
@@ -62,28 +85,34 @@ class MixedDimensionalGrid(pp.MixedDimensionalGrid):
         )
 
     def num_subdomain_ridges(self, cond=None) -> int:
-        """Compute the total number ridges of of the mixed-dimensional grid.
+        """
+        Compute the total number of ridges in the mixed-dimensional grid.
 
         A function can be passed to filter subdomains and/or interfaces.
 
         Args:
-            cond: optional, predicate with a grid as input.
+            cond: Optional. A predicate function that takes a grid as input.
 
-        Return:
-            int: the total number of faces of the mixed-dimensional grid.
-
+        Returns:
+            int: The total number of ridges in the mixed-dimensional grid.
         """
         if cond is None:
             cond = lambda _: True
-        return np.sum(  # type: ignore
+        return np.sum(
             [sd.num_ridges for sd in self.subdomains() if cond(sd)], dtype=int
         )
 
-    def tag_leafs(self):
+    def tag_leafs(self) -> None:
         """
         Tag the mesh entities that correspond to a mesh entity of a lower-dimensional
         grid in a grid bucket.
         TODO: Use these tags to generate mixed-dimensional inner products.
+
+        Args:
+            None
+
+        Returns:
+            None
         """
 
         for sd in self.subdomains():
