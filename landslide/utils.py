4a1c6fb33774a182acef50f8ff57b8f7653ce499
Federico Gatti <federico@MacBook-Air-di-IMATI-5.local>
2024-10-09 14:10:08 +0200

    last version

--- landslide/utils.py
+++ landslide/utils.py
@@ -1,7 +1,9 @@
 import time
+from typing import Callable, Optional, Union
 import numpy as np
 import scipy.sparse as sps
 import porepy as pp 
+from topograhy_file import theta, conductivity, DthetaDpsi#, DConductivityDpsi
 
 def TicTocGenerator():
     # Generator that returns time differences
@@ -26,7 +28,7 @@ def tic():
     toc(False)
 
 
-def assembe_inv_preconditioner(subdomain, RT0, p0p0_mass, data) -> sps.csc_matrix:
+def assembe_inv_preconditioner(subdomain, dof_q, dof_psi, RT0, p0p0_mass, data) -> sps.csc_matrix:
         """
         Assembles the lumped mass matrix L such that
         B^T L^{-1} B is a TPFA method.
@@ -40,7 +42,7 @@ def assembe_inv_preconditioner(subdomain, RT0, p0p0_mass, data) -> sps.csc_matri
         """
         lumped_mass_u = RT0.assemble_lumped_matrix(subdomain, data)
 
-        return sps.diags(np.concatenate([1./lumped_mass_u.data, 1./p0p0_mass.data])).tocsc()
+        return sps.diags(np.concatenate([1./(lumped_mass_u.A[np.arange(dof_q), np.arange(dof_q)]+1e-13), 1./(p0p0_mass.A[np.arange(dof_psi), np.arange(dof_psi)]+1e-13)])).tocsc()
 
 
 
@@ -55,13 +57,16 @@ class Nitsche_term:
 
         self.flux_top_bc = np.zeros(dof_q)
 
-        self.gamma_Nitsche = gamma_Nitsche
+        self.gamma_Nitsche_0 = gamma_Nitsche
 
         self.subdomain = subdomain
 
-        self.pressure_head_thr = 0
+        self.pressure_head_thr = 0e-2
 
     def compute_Nitsche_contributions(self, gamma_topography, prec_proj, prev, normal_vector_top):
+
+        #self.gamma_Nitsche = self.gamma_Nitsche_0*conductivity(prev[-self.dof_psi:][self.subdomain.cell_faces[gamma_topography].nonzero()[1]]/self.subdomain.cell_volumes[self.subdomain.cell_faces[gamma_topography].nonzero()[1]])
+        self.gamma_Nitsche = self.gamma_Nitsche_0*1e-10
 
         Nitsche_argument = (prec_proj[gamma_topography] - prev[:self.dof_q][gamma_topography])*normal_vector_top/self.subdomain.face_areas[gamma_topography] - self.gamma_Nitsche*(prev[-self.dof_psi:][self.subdomain.cell_faces[gamma_topography].nonzero()[1]]/self.subdomain.cell_volumes[self.subdomain.cell_faces[gamma_topography].nonzero()[1]]-self.pressure_head_thr)
         
@@ -80,16 +85,19 @@ class Nitsche_term_hybrid:
 
         self.flux_top_bc = np.zeros(dof_l)
 
-        self.gamma_Nitsche = gamma_Nitsche
+        self.gamma_Nitsche_0 = gamma_Nitsche
 
         self.subdomain = subdomain
 
-        self.pressure_head_thr = 0
+        self.pressure_head_thr = 0e-2
 
     def compute_Nitsche_contributions(self, gamma_topography, prec_proj, prev, normal_vector_top):
+        
+        #self.gamma_Nitsche = self.gamma_Nitsche_0/conductivity(prev[-self.dof_l:]/self.subdomain.face_areas[gamma_topography])
+        self.gamma_Nitsche = self.gamma_Nitsche_0*1e0
 
         Nitsche_argument = (prev[-self.dof_l:]/self.subdomain.face_areas[gamma_topography]-self.pressure_head_thr) - self.gamma_Nitsche*(prec_proj[gamma_topography] - prev[:self.dof_q][gamma_topography])*normal_vector_top/self.subdomain.face_areas[gamma_topography]
-        # 1./(gamma_Nitsche*1e7)*np.maximum(0,  ) 
+        #Nitsche_argument =  (prev[-self.dof_l:]/self.subdomain.face_areas[gamma_topography]-self.pressure_head_thr)
         
         #self.matrix_Nitsche_ll[np.arange(self.dof_l), np.arange(self.dof_l)] = (Nitsche_argument>0)*(-1./self.subdomain.face_areas[gamma_topography])
         #self.matrix_Nitsche_lq[np.arange(self.dof_l), gamma_topography     ] = (Nitsche_argument>0)*(-normal_vector_top/self.subdomain.face_areas[gamma_topography])
@@ -110,11 +118,11 @@ class Matrix_Computer:
         self.dof_psi = dof_psi
 
         # compute mesh properties useful to compute the RT0 mass matrix
-        cells_id = np.arange(0, dof_psi)
-        self.faces_id = sd.cell_faces[:,cells_id].indices
+        self.cells_id = np.arange(0, dof_psi)
+        self.faces_id = sd.cell_faces[:,self.cells_id].indices
         self.faces_id = np.reshape(self.faces_id, (dof_psi, 3))
 
-        faces_val = sd.cell_faces[:,cells_id].data
+        faces_val = sd.cell_faces[:,self.cells_id].data
         faces_val = np.reshape(faces_val, (dof_psi, 3))
 
         nodes_f0_id = np.reshape(sd.face_nodes[:,self.faces_id[:,0]].indices, (dof_psi, 2))
@@ -145,7 +153,7 @@ class Matrix_Computer:
         self.xx_m1 = np.array([x_m1, y_m1])
         self.xx_m2 = np.array([x_m2, y_m2])
 
-        # RT= basis functions
+        # RT0 basis functions
         self.base_func_f0 = lambda x: .5*(x - nodes_coord_p0)*faces_val[:,0]/sd.cell_volumes
         self.base_func_f1 = lambda x: .5*(x - nodes_coord_p1)*faces_val[:,1]/sd.cell_volumes
         self.base_func_f2 = lambda x: .5*(x - nodes_coord_p2)*faces_val[:,2]/sd.cell_volumes
@@ -174,7 +182,9 @@ class Matrix_Computer:
         self.col_index = np.concatenate((self.faces_id[:,0], self.faces_id[:,1], self.faces_id[:,2], self.faces_id[:,0], self.faces_id[:,1], self.faces_id[:,2], self.faces_id[:,0], self.faces_id[:,1], self.faces_id[:,2]))
 
         self.row_index_dual_C = np.concatenate((self.faces_id[:,0], self.faces_id[:,1], self.faces_id[:,2]))
-        self.col_index_dual_C = np.concatenate((cells_id, cells_id, cells_id))
+        self.col_index_dual_C = np.concatenate((self.cells_id, self.cells_id, self.cells_id))
+
+        self.is_newton_scheme = False
 
         #sps.coo_matrix((np.ones(9*dof_psi), (self.row_index, self.col_index))).tocsc()
         #mass_matrix_values = np.concatenate((self.contr_00, self.contr_01, self.contr_02, self.contr_01, self.contr_11, self.contr_12, self.contr_02, self.contr_12, self.contr_22))
@@ -195,10 +205,77 @@ class Matrix_Computer:
         # A.toarray()[63][A.toarray()[63]>0]
         # Mass_u.toarray()[63][Mass_u.toarray()[63]>0]
 
+    def set_L_coefficients(self, initial_solution):
+        self.L_scheme_coeff_M = np.maximum(1*np.max(DthetaDpsi(initial_solution)), 1e-6)
+        self.L_scheme_coeff_m = self.L_scheme_coeff_M/8.
+        self.coeff_LL = np.sqrt(2.)
+        self.L_scheme_coeff = self.L_scheme_coeff_m
+
+        #self.L_scheme_coeff = self.L_scheme_coeff_M
+
+        self.eta_lin_old_old = 0.
+        self.eta_lin_old = 0.
+        self.eta_lin = 0.
+
+        self.eta_ll_old_old = 0.
+        self.eta_ll_old = 0.
+        self.eta_ll = 0.
+
+    def update_L_coefficients(self, sd, tau, psi, psi_it_old, psi_it_old_old, q_it, q_it_old):
+
+        self.eta_lin_old_old = self.eta_lin_old
+        self.eta_lin_old = self.eta_lin
+        self.eta_lin = self.compute_energy_norm(tau, sd, psi, psi_it_old, q_it, q_it_old)
+
+        eta_square_poten = np.sum(sd.cell_volumes*(1./np.sqrt(self.L_scheme_coeff)*(self.L_scheme_coeff*(psi_it_old - psi_it_old_old) -  (theta(psi_it_old) - theta(psi_it_old_old))))*(1./np.sqrt(self.L_scheme_coeff)*(self.L_scheme_coeff*(psi_it_old - psi_it_old_old) -  (theta(psi_it_old) - theta(psi_it_old_old)))))
+        eta_square_flux  = np.dot(q_it_old, self.compute_RT0_mass_matrix((conductivity(psi_it_old) - conductivity(psi_it_old_old))*(conductivity(psi_it_old) - conductivity(psi_it_old_old))/(conductivity(psi_it_old)*conductivity(psi_it_old)*conductivity(psi_it_old)))*q_it_old) 
+        self.eta_ll_old_old = self.eta_ll_old
+        self.eta_ll_old = self.eta_ll
+        self.eta_ll = np.sqrt(eta_square_poten + tau*eta_square_flux)
+
+        if self.eta_ll > self.eta_lin:
+            self.L_scheme_coeff_m = self.L_scheme_coeff
+            self.L_scheme_coeff = np.minimum(self.coeff_LL*self.L_scheme_coeff, self.L_scheme_coeff_M)
+        elif self.eta_ll > .8*self.eta_lin and self.eta_ll_old > .8*self.eta_lin_old and self.eta_ll_old_old > .8*self.eta_lin_old_old:
+            self.L_scheme_coeff = np.maximum(.9*self.L_scheme_coeff, 1.1*self.L_scheme_coeff_m)
+        #elif self.eta_ll < 10*self.eta_lin:
+        #    self.L_scheme_coeff = 1e3*self.L_scheme_coeff_M
+        #    self.L_scheme_coeff_M *= self.coeff_LL
+            
+        #self.L_scheme_coeff = self.L_scheme_coeff_M
+        
+
+    def compute_energy_norm(self, tau, sd, psi, psi_it_old, q_it, q_it_old):
+        delta_psi = psi-psi_it_old
+        if self.is_newton_scheme:
+            res = np.sum(DthetaDpsi(psi_it_old)*delta_psi*delta_psi*sd.cell_volumes)
+        else:
+            res = np.sum(self.L_scheme_coeff*delta_psi*delta_psi*sd.cell_volumes)
+
+        res += tau*np.dot(q_it, self.compute_RT0_mass_matrix(conductivity(psi_it_old)/(conductivity(psi)*conductivity(psi)))*q_it)
+        res += tau*np.dot(q_it_old, self.compute_RT0_mass_matrix(1./conductivity(psi_it_old))*q_it_old)
+        res -= tau*np.dot(q_it_old, self.compute_RT0_mass_matrix(1./conductivity(psi))*q_it)*2
+
+        if res<1e-10:
+            res = np.abs(abs(res))
+        
+        #if np.isnan(np.sqrt(res)):
+        #    print(res)
+        
+        res = np.sqrt(res)
+        
+        return res
+    
+    #def compute_C_n_coefficient(self, tau, psi_it_old, q_it):
+    #    tau*DConductivityDpsi(psi_it_old)*DConductivityDpsi(psi_it_old)/(conductivity(psi_it_old)*conductivity(psi_it_old)*conductivity(psi_it_old))*np.dot(q_it, q_it)
 
     def compute_RT0_mass_matrix(self, cond_coeff):
-        mass_matrix_values = np.concatenate((self.contr_00/cond_coeff, self.contr_01/cond_coeff, self.contr_02/cond_coeff, self.contr_01/cond_coeff, self.contr_11/cond_coeff, self.contr_12/cond_coeff, self.contr_02/cond_coeff, self.contr_12/cond_coeff, self.contr_22/cond_coeff))
+        mass_matrix_values = np.concatenate((self.contr_00*cond_coeff, self.contr_01*cond_coeff, self.contr_02*cond_coeff, self.contr_01*cond_coeff, self.contr_11*cond_coeff, self.contr_12*cond_coeff, self.contr_02*cond_coeff, self.contr_12*cond_coeff, self.contr_22*cond_coeff))
         return sps.coo_matrix((mass_matrix_values, (self.row_index, self.col_index)), shape=(self.dof_q, self.dof_q)).tocsc()
+    
+    #def compute_P0_mass_matrix(self, cond_coeff):
+    #    mass_matrix_values = np.concatenate((self.contr_00*cond_coeff, self.contr_01*cond_coeff, self.contr_02*cond_coeff, self.contr_01*cond_coeff, self.contr_11*cond_coeff, self.contr_12*cond_coeff, self.contr_02*cond_coeff, self.contr_12*cond_coeff, self.contr_22*cond_coeff))
+    #    return sps.coo_matrix((mass_matrix_values, (self.row_index, self.col_index)), shape=(self.dof_q, self.dof_q)).tocsc()
 
     def compute_dual_C(self, sd, q, cond_coeff):
 
@@ -218,5 +295,35 @@ class Matrix_Computer:
         contr_2 = (f_2(self.xx_m0) + f_2(self.xx_m1) + f_2(self.xx_m2))/3.*sd.cell_volumes
 
         mass_matrix_values = np.concatenate((contr_0*cond_coeff/sd.cell_volumes, contr_1*cond_coeff/sd.cell_volumes, contr_2*cond_coeff/sd.cell_volumes))
-        return sps.coo_matrix((mass_matrix_values, (self.row_index_dual_C, self.col_index_dual_C)), shape=(self.dof_q, self.dof_psi)).tocsc()
+
+        if self.is_newton_scheme:
+            return sps.coo_matrix((mass_matrix_values, (self.row_index_dual_C, self.col_index_dual_C)), shape=(self.dof_q, self.dof_psi)).tocsc()
+        else:
+            return sps.dok_matrix((self.dof_q, self.dof_psi)) 
+        
+    def compute_D_res(self, sd, q, cond_coeff):
+
+        #
+        q_0 = q[self.faces_id[:,0]]
+        q_1 = q[self.faces_id[:,1]]
+        q_2 = q[self.faces_id[:,2]]
+
+        # diagonal integrands
+        f = lambda x: (q_0*self.base_func_f0(x)[0] + q_1*self.base_func_f1(x)[0] + q_2*self.base_func_f2(x)[0])*(q_0*self.base_func_f0(x)[0] + q_1*self.base_func_f1(x)[0] + q_2*self.base_func_f2(x)[0]) + (q_0*self.base_func_f0(x)[1] + q_1*self.base_func_f1(x)[1] + q_2*self.base_func_f2(x)[1])*(q_0*self.base_func_f0(x)[1] + q_1*self.base_func_f1(x)[1] + q_2*self.base_func_f2(x)[1])
+
+        # computing integrals
+        contr = (f(self.xx_m0) + f(self.xx_m1) + f(self.xx_m2))/3.*sd.cell_volumes
+
+        mass_matrix_values = contr*cond_coeff/(sd.cell_volumes*sd.cell_volumes)
+
+        if self.is_newton_scheme:
+            return sps.coo_matrix((mass_matrix_values, (self.cells_id, self.cells_id)), shape=(self.dof_psi, self.dof_psi)).tocsc()
+        else:
+            return sps.dok_matrix((self.dof_psi, self.dof_psi))
+    
+    def compute_P0_mass_matrix(self, sd, data, flag: Optional[bool] = False):
+        if self.is_newton_scheme or flag:
+            return sps.diags(data* 1./sd.cell_volumes).tocsc() 
+        else: 
+            return sps.diags(self.L_scheme_coeff* 1./sd.cell_volumes).tocsc() 
 
