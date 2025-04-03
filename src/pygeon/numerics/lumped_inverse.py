15951ffc4c644dc41c1f2cd152ae7b3f7cbbdfc5
alessio.fumagalli <alessio.fumagalli1984@gmail.com>
2025-04-03 15:33:44 +0200

    add comments to the block diag

--- src/pygeon/numerics/lumped_inverse.py
+++ src/pygeon/numerics/lumped_inverse.py
@@ -53,47 +53,53 @@ def block_diag_solver(M: sps.csc_array, B: sps.csc_array) -> sps.csc_array:
     """
     Solves a block diagonal system of linear equations for each connected component.
 
-    This function takes a sparse block diagonal matrix `M` and a right-hand side vector or matrix `b`,
-    and solves the system `Mx = b` for each connected component of the matrix `M`. The connected
-    components are determined using a graph representation of the matrix.
+    This function takes a sparse block diagonal matrix M assumed to be symmetric and
+    positive defined and a right-hand side matrix B, and solves the system MX = B for
+    each connected component of the matrix M.
 
-    Parameters:
-        M (sps.csc_array): A sparse matrix in Compressed Sparse Column (CSC) format, representing
-            the block diagonal system. It is assumed to be symmetric and positive definite.
-        b (Union[np.ndarray, sps.csc_array]): The right-hand side vector or matrix. It can be either
-            a dense NumPy array or a sparse CSC array.
+    The connected components are determined using a graph representation of the matrix.
+
+    Args:
+        M (sps.csc_array): A symmetric and positive defined sparse matrix in Compressed
+            Sparse Column (CSC) format, representing the block diagonal system. It is
+            assumed to be symmetric and positive definite.
+        B (sps.csc_array): The right-hand side matrix in Compressed Sparse Column (CSC)
+            format.
 
     Returns:
-        Union[np.ndarray, sps.csc_array]: The solution vector or matrix `x` in the same format as `b`.
-            If `b` is a dense NumPy array, the solution will be a NumPy array. If `b` is a sparse
-            CSC array, the solution will also be returned as a sparse CSC array.
+        sps.csc_array: The solution matrix X.
 
     Notes:
-        - The function identifies connected components of the matrix `M` using the `connected_components`
-          function from `scipy.sparse.csgraph`.
-        - For each connected component, the corresponding submatrix and subvector are extracted, and
-          the system is solved using `numpy.linalg.solve`.
-        - If the right-hand side `b` is sparse and contains zero entries for a connected component,
-          the solution for that component is skipped.
+        - The function identifies connected components of the matrix M using the
+          connected_components function from `scipy.sparse.csgraph`.
+        - For each connected component, the corresponding submatrix and subvector are
+          extracted, and the system is solved using `numpy.linalg.solve`.
+        - If the right-hand side B is sparse and contains zero entries for a connected
+          component, the solution for that component is skipped.
     """
     # Get connected components
     n_components, labels = csgraph.connected_components(M, directed=False)
 
+    # Convert M and B to LIL format for efficient row and column access
     M_lil = M.tolil()
     B_lil = B.tolil()
     sol = sps.lil_array(B.shape)
 
+    # Iterate over each connected component of the matrix M
     for patch in np.arange(n_components):
         # Get the indices of the connected component
         rows = np.where(labels == patch)[0]
 
+        # Create a submatrix for the connected component
         sub_B = B_lil[rows, :]
+        # Get the non-zero columns of the submatrix
         cols = np.unique(np.nonzero(sub_B)[1])
 
+        # If there are no non-zero columns, skip this component
         if not np.any(cols):
             continue
 
-        # Create a submatrix for the connected component
+        # Create a dense submatrix for the connected component
         sub_M = M_lil[np.ix_(rows, rows)].toarray()
 
         # Solve the dense system and distribute to the solution matrix
@@ -106,15 +112,35 @@ def block_diag_solver(M: sps.csc_array, B: sps.csc_array) -> sps.csc_array:
 
 
 def block_diag_solver_dense(M: sps.csc_array, b: np.ndarray) -> np.ndarray:
-    b_is_vector = b.ndim == 1
+    """
+    Solves a system of linear equations where the coefficient matrix M is symmetric
+    and positive definite block diagonal, and the right-hand side is given by b a
+    numpy array.
 
-    if b_is_vector:
+    Args:
+        M (sps.csc_array): A symmetric and positive definite sparse matrix in Compressed
+            Sparse Column (CSC) format representing the block diagonal coefficient
+            matrix.
+        b (np.ndarray): The right-hand side of the equation. Can be a 1D array (vector)
+            or a 2D array (matrix). If 1D, it will be treated as a column vector.
+
+    Returns:
+        np.ndarray: The solution to the system of equations. If b is a 1D array, the
+            solution will also be returned as a 1D array. If b is a 2D array, the
+            solution will be returned as a 2D array.
+    """
+    # If b is a 1D array, convert it to a 2D column vector
+    # This is necessary for the solver to work correctly
+    if b.ndim == 1:
         b = np.atleast_2d(b).T
 
+    # Transform the right hand side to a sparse matrix
     b_csc = sps.csc_array(b)
+    # Compute the solution by using the block diagonal solver
     sol = block_diag_solver(M, b_csc).toarray()
 
-    if b_is_vector:
+    # If b was a 1D array, convert the solution back to a 1D array
+    if b.ndim == 1:
         sol = np.squeeze(sol)
 
     return sol
