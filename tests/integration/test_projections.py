53391fb73d6167dd6cff38f7c7bd22d6e51b5e2b
Wietse Boon <wietseboon@gmail.com>
2025-08-05 16:37:03 +0200

    rewrote projection tests using fixtures

--- tests/integration/test_projections.py
+++ tests/integration/test_projections.py
@@ -1,47 +1,69 @@
-import unittest
-
 import numpy as np
+import pytest
 
 import pygeon as pg
 
 
-class ProjectionsTest(unittest.TestCase):
-    def test_lagrange1(self):
-        for dim in [1, 2, 3]:
-            for degree in [1, 2]:
-                sd = pg.unit_grid(0.5, dim, as_mdg=False)
-                sd.compute_geometry()
+@pytest.fixture(params=[1, 2, 3])
+def sd(request):
+    dim = request.param
+    sd = pg.unit_grid(dim, 0.25, as_mdg=False)
+    sd.compute_geometry()
 
-                l1 = pg.Lagrange1()
-                l1_mass = l1.assemble_mass_matrix(sd)
+    return sd
 
-                pi = pg.proj_to_PwPolynomials(l1, sd, degree)
 
-                poly = pg.get_PwPolynomials(degree, pg.SCALAR)()
-                poly_mass = poly.assemble_mass_matrix(sd)
+@pytest.fixture(
+    params=[
+        pg.Lagrange1,
+        pg.Lagrange2,
+        pg.RT0,
+        pg.BDM1,
+        pg.RT1,
+        pg.PwConstants,
+        pg.PwLinears,
+        pg.PwQuadratics,
+    ]
+)
+def disc(request):
+    return request.param("test")
 
-                diff = pi.T @ poly_mass @ pi - l1_mass
 
-                self.assertTrue(np.allclose(diff.data, 0))
+def test_mass_matrix(sd, disc):
+    poly_order = disc.poly_order
+    tensor_order = disc.tensor_order
 
-    def test_rt0(self):
-        for dim in [1, 2, 3]:
-            for degree in [1, 2]:
-                sd = pg.unit_grid(0.5, dim, as_mdg=False)
-                sd.compute_geometry()
+    mass = disc.assemble_mass_matrix(sd)
+    pi = pg.proj_to_PwPolynomials(disc, sd, poly_order)
 
-                rt0 = pg.RT0()
-                rt0_mass = rt0.assemble_mass_matrix(sd)
+    poly = pg.get_PwPolynomials(poly_order, tensor_order)()
+    poly_mass = poly.assemble_mass_matrix(sd)
 
-                pi = pg.proj_to_PwPolynomials(rt0, sd, degree)
+    diff = pi.T @ poly_mass @ pi - mass
 
-                poly = pg.get_PwPolynomials(degree, pg.VECTOR)()
-                poly_mass = poly.assemble_mass_matrix(sd)
+    assert np.allclose(diff.data, 0)
 
-                diff = pi.T @ poly_mass @ pi - rt0_mass
 
-                self.assertTrue(np.allclose(diff.data, 0))
+def test_lumped_matrix(sd, disc):
+    if isinstance(disc, pg.RT0):
+        # The RT0 lumped matrix does not coincide with the
+        # one from the piecewise polynomial interpretation
+        # so this test is skipped
+        return
+
+    poly_order = disc.poly_order
+    tensor_order = disc.tensor_order
+
+    lumped = disc.assemble_lumped_matrix(sd)
+    pi = pg.proj_to_PwPolynomials(disc, sd, poly_order)
+
+    poly = pg.get_PwPolynomials(poly_order, tensor_order)()
+    poly_lumped = poly.assemble_lumped_matrix(sd)
+
+    diff = pi.T @ poly_lumped @ pi - lumped
+
+    assert np.allclose(diff.data, 0)
 
 
 if __name__ == "__main__":
-    unittest.main()
+    pytest.main([__file__])
