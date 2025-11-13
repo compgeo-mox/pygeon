09cc9b6c6030bcd4443940555e83194158e89bda
alessio.fumagalli <alessio.fumagalli1984@gmail.com>
2025-11-13 19:37:11 +0100

    remove additional useless import

--- tests/discretizations/fem/vec_h1/test_veclagrange1.py
+++ tests/discretizations/fem/vec_h1/test_veclagrange1.py
@@ -3,7 +3,6 @@
 import numpy as np
 import porepy as pp
 import pytest
-import scipy.sparse as sps
 
 import pygeon as pg
 
--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,7 +1,5 @@
 """Module contains a dummy unit test that always passes."""
 
-import pytest
-
 
 def test_dummy():
     assert True
