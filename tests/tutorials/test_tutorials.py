0c86341c3e789d52e1f36d286fa22f2a7a048958
alessio.fumagalli <alessio.fumagalli1984@gmail.com>
2025-12-01 18:02:12 +0100

    try to change the test tutorial to avoid some cells that are marked

--- tests/tutorials/test_tutorials.py
+++ tests/tutorials/test_tutorials.py
@@ -2,53 +2,78 @@
 import glob
 import os
 import sys
+import types
 
+import nbformat
 import pytest
+from nbconvert import NotebookExporter
+from nbconvert.preprocessors import Preprocessor
 
 TUTORIAL_FILENAMES = glob.glob("tutorials/*.ipynb")
+
+
+class IgnoreCommentPreprocessor(Preprocessor):
+    """
+    Remove any code cell whose first line starts with '# NBIGNORE'
+    """
+
+    def preprocess(self, nb, resources):
+        new_cells = []
+        for cell in nb.cells:
+            if cell.cell_type == "code" and cell.source.strip().startswith(
+                "# NBIGNORE"
+            ):
+                continue
+            new_cells.append(cell)
+        nb.cells = new_cells
+        return nb, resources
 
 
 @pytest.mark.tutorials
 @pytest.mark.parametrize("tutorial_path", TUTORIAL_FILENAMES)
 def test_run_tutorials(tutorial_path: str):
-    """We run the tutorial and check that it didn't raise any error.
-    This assumes we run pytest from the pygeon directory.
-
-    """
+    """Run the tutorial and check that it didn't raise any error."""
     new_file = tutorial_path.removesuffix(".ipynb") + ".py"
 
-    # This command might fail in github actions.
-    cmd_convert = "jupyter-nbconvert --to script " + tutorial_path
-    status = os.system(cmd_convert)
-    if status != 0:
-        raise RuntimeError(
-            ".ipynb file is not converted to .py file. Is jupyter-nbconvert available?"
-        )
+    # --- Convert notebook to script with IgnoreCommentPreprocessor ---
+    nb = nbformat.read(tutorial_path, as_version=4)
+    exporter = NotebookExporter()
+    exporter.register_preprocessor(IgnoreCommentPreprocessor, enabled=True)
+    body, _ = exporter.from_notebook_node(nb)
+
+    with open(new_file, "w", encoding="utf-8") as f:
+        f.write(body)
+
+    # --- Patch imports and disable interactive plotting ---
     edit_imports(new_file)
 
-    cmd_run = "python " + str(new_file)
+    # --- Run the generated Python script ---
+    cmd_run = f"python {new_file}"
     status = os.system(cmd_run)
-
     assert status == 0
 
-    # Removing the generated source file after the assertion. If the test fails, it is
-    # useful to keep it in order to see what went wrong there.
+    # --- Clean up generated script ---
     os.remove(new_file)
 
 
 def edit_imports(filename: str):
-    """Matplotlib opens a new window for each figure in the interactive mode.
-    Here, we prevent it by setting the noninteractive matplotlib backend.
-    "template" backend is a dummy backend that does nothing.
-    Also, we cd to the tutorials directory.
-
-    """
+    """Patch the generated script to avoid opening windows with Matplotlib/VTK."""
     with open(filename, encoding="utf-8") as f:
         content = f.readlines()
 
     with open(filename, "w", encoding="utf-8") as f:
-        f.write("import os; os.chdir('./tutorials')\n")
+        # Disable VTK/PyVista offscreen rendering
+        f.write("import os\n")
+        f.write("os.environ['PYVISTA_OFF_SCREEN'] = 'true'\n")
+        f.write("os.environ['PYVISTA_USE_PANEL'] = 'false'\n")
+        f.write("os.environ.pop('DISPLAY', None)\n")
+
+        # Ensure we are in tutorials directory
+        f.write("os.chdir('./tutorials')\n")
+
+        # Disable matplotlib windows
         f.write("import matplotlib; matplotlib.use('template')\n")
+
         f.writelines(content)
 
 
