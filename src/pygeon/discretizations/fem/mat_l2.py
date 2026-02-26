53d6b017ba7c7afa33064d43bc2ebcde5f3c2231
Wietse M. Boon <35607918+wmboon@users.noreply.github.com>
2026-02-26 16:04:57 +0100

    Refactor asym, trace, and mult_mat (#223)
    * refactor using kronecker products
    
    * improved docstrings and removed useless test
    
    * moved assemble_mult_matrix to the MatPwP level
    
    * updated test
    
    * fixed mistake in mult_mat and added dedicated test
    
    * csc output of assemble_mult_matrix
    
    * added coverage test

--- src/pygeon/discretizations/fem/mat_l2.py
+++ src/pygeon/discretizations/fem/mat_l2.py
@@ -207,15 +207,19 @@ class MatPwPolynomials(pg.VecPwPolynomials):
             sd (pg.Grid): The grid.
 
         Returns:
-            sps.csc_array: The trace matrix obtained from the discretization.
-
-        Raises:
-            NotImplementedError: This method is not implemented for the base class.
+            sps.csc_array: The trace matrix.
         """
-        raise NotImplementedError(
-            "The trace matrix assembly is not implemented for the base class. "
-            "Please use the specific implementations in the subclasses."
-        )
+        # Extract the number of degrees of freedom for the underlying scalar space.
+        scalar_ndof = self.ndof(sd) // (sd.dim**2)
+
+        # The trace of a dxd matrix M is a linear operator acting on M.ravel(). This
+        # linear operator is given by the following matrix:
+        # 1D: [1]
+        # 2D: [1 0 0 1]
+        # 3D: [1 0 0 0 1 0 0 0 1]
+        trace = np.eye(sd.dim).reshape((1, -1))
+
+        return sps.kron(trace, sps.eye_array(scalar_ndof), format="csc")
 
     def assemble_asym_matrix(self, sd: pg.Grid) -> sps.csc_array:
         """
@@ -226,15 +230,75 @@ class MatPwPolynomials(pg.VecPwPolynomials):
             sd (pg.Grid): The grid.
 
         Returns:
-            sps.csc_array: The asymmetry matrix obtained from the discretization.
-
-        Raises:
-            NotImplementedError: This method is not implemented for the base class.
+            sps.csc_array: The asymmetry matrix.
         """
-        raise NotImplementedError(
-            "The asymmetry matrix assembly is not implemented for the base class. "
-            "Please use the specific implementations in the subclasses."
-        )
+        # Extract the number of degrees of freedom for the underlying scalar space.
+        scalar_ndof = self.ndof(sd) // (sd.dim**2)
+
+        # The asymmetry of a dxd matrix M is a linear operator acting on M.ravel(). This
+        # linear operator is given by the following matrix:
+        match sd.dim:
+            case 2:
+                asym = np.array([[0, -1, 1, 0]])
+            case 3:
+                # [0  0 0 0 0 -1  0 1 0]
+                # [0  0 1 0 0  0 -1 0 0]
+                # [0 -1 0 1 0  0  0 0 0]
+                asym = np.zeros((3, 9))
+                asym[[0, 1, 2], [7, 2, 3]] = 1
+                asym[[0, 1, 2], [5, 6, 1]] = -1
+            case _:
+                raise ValueError("The grid should be either two or three-dimensional")
+
+        return sps.kron(asym, sps.eye_array(scalar_ndof), format="csc")
+
+    def assemble_mult_matrix(
+        self, sd: pg.Grid, mult_mat: np.ndarray, right_mult: bool
+    ) -> sps.csc_array:
+        """
+        Assembles and returns the matrix that multiplies with an elementwise
+        matrix-valued function.
+
+        Args:
+            sd (pg.Grid): The grid.
+            mult_mat (np.ndarray): The matrix to multiply with. It is assumed to be
+                a piecewise constant matrix and can be provided with shape
+                (d, d, n_cells), (d, d, n_dof), or their flattened equivalents.
+            right_mult (bool): If True, performs right multiplication. If False, left
+                multiplication.
+
+        Returns:
+            sps.csc_array: The multiplication matrix obtained from the discretization.
+        """
+        # Reshape the multiplication matrix so that we can easily access its components
+        # per cell.
+        mult_mat = mult_mat.reshape((sd.dim, sd.dim, -1))
+
+        # If one matrix is given per element, then we need to tile its values according
+        # to the number of degrees of freedom per cell.
+        if mult_mat.shape[-1] * self.ndof_per_cell(sd) == self.ndof(sd):
+            mult_mat = np.tile(mult_mat, self.ndof_per_cell(sd) // (sd.dim**2))
+
+        # We create a sparse matrix with diagonal blocks based on the entries in
+        # mult_mat.
+        identity = np.eye(sd.dim)
+        blocks = [
+            [sps.diags_array(mult_ij) for mult_ij in mult_i] for mult_i in mult_mat
+        ]
+
+        if right_mult:
+            # Right multiplication is achieved by first assembling the block matrix and
+            # then applying a Kronecker product to the transpose.
+            mult_array = sps.block_array(blocks)
+            return sps.kron(identity, mult_array.T, format="csc")
+        else:
+            # Left multiplication is achieved by first applying a Kronecker product and
+            # then assembling the block matrix.
+            tiled_blocks = [
+                [sps.kron(identity, block) for block in block_row]
+                for block_row in blocks
+            ]
+            return sps.block_array(tiled_blocks, format="csc")
 
 
 class MatPwConstants(MatPwPolynomials):
@@ -284,100 +348,6 @@ class MatPwLinears(MatPwPolynomials):
         super().__init__(keyword)
         self.base_discr = pg.VecPwLinears(keyword)
 
-    def assemble_trace_matrix(self, sd: pg.Grid) -> sps.csc_array:
-        """
-        Assembles and returns the trace matrix for the matrix-valued piecewise linears.
-
-        Args:
-            sd (pg.Grid): The grid.
-
-        Returns:
-            sps.csc_array: The trace matrix obtained from the discretization.
-        """
-        num_int_points = self.ndof_per_cell(sd) // (sd.dim**2)
-
-        size = num_int_points * sd.dim * sd.num_cells
-        rows_I = np.empty(size, dtype=int)
-        cols_J = np.empty(size, dtype=int)
-        data_IJ = np.ones(size)
-        idx = 0
-
-        if sd.dim == 1:
-            mask = [0, 1]
-        elif sd.dim == 2:
-            mask = [0, 1, 2, 9, 10, 11]
-        elif sd.dim == 3:
-            mask = [0, 1, 2, 3, 16, 17, 18, 19, 32, 33, 34, 35]
-
-        range_disc = pg.PwLinears()
-
-        for c in range(sd.num_cells):
-            loc_dofs = self.local_dofs_of_cell(sd, c)[mask]
-
-            ran_dofs = range_disc.local_dofs_of_cell(sd, c)
-            ran_dofs = np.tile(ran_dofs, sd.dim)
-
-            # Save values of the local matrix in the global structure
-            loc_idx = slice(idx, idx + loc_dofs.size)
-            rows_I[loc_idx] = ran_dofs
-            cols_J[loc_idx] = loc_dofs
-            idx += loc_dofs.size
-
-        # Construct the global matrices
-        shape = (range_disc.ndof(sd), self.ndof(sd))
-        return sps.csc_array((data_IJ, (rows_I, cols_J)), shape=shape)
-
-    def assemble_asym_matrix(self, sd: pg.Grid) -> sps.csc_array:
-        """
-        Assembles and returns the asymmetry matrix for the matrix-valued
-        piecewise linears.
-
-        Args:
-            sd (pg.Grid): The grid.
-
-        Returns:
-            sps.csc_array: The asymmetry matrix obtained from the discretization.
-        """
-        num_int_points = self.ndof_per_cell(sd) // (sd.dim**2)
-        size = num_int_points * (sd.dim * (sd.dim - 1)) * sd.num_cells
-        rows_I = np.empty(size, dtype=int)
-        cols_J = np.empty(size, dtype=int)
-        data_IJ = np.empty(size)
-        idx = 0
-
-        range_disc: pg.PwLinears | pg.VecPwLinears
-
-        if sd.dim == 2:
-            mask = np.arange(3, 9)
-            loc_data = np.repeat([-1, 1], 3)
-            range_disc = pg.PwLinears()
-            rearrange = np.tile(np.arange(range_disc.ndof_per_cell(sd)), 2)
-
-        elif sd.dim == 3:
-            mask = np.hstack((np.arange(4, 16), np.arange(20, 32)))
-            loc_data = np.repeat([-1, 1, 1, -1, -1, 1], 4)
-            range_disc = pg.VecPwLinears()
-
-            rearrange = np.arange(12).reshape((3, 4))
-            rearrange = rearrange[[2, 1, 2, 0, 1, 0]].ravel()
-        else:
-            raise ValueError("The grid should be either two or three-dimensional")
-
-        for c in range(sd.num_cells):
-            loc_dofs = self.local_dofs_of_cell(sd, c)[mask]
-            ran_dofs = range_disc.local_dofs_of_cell(sd, c)[rearrange]
-
-            # Save values of the local matrix in the global structure
-            loc_idx = slice(idx, idx + loc_dofs.size)
-            rows_I[loc_idx] = ran_dofs
-            cols_J[loc_idx] = loc_dofs
-            data_IJ[loc_idx] = loc_data
-            idx += loc_dofs.size
-
-        # Construct the global matrices
-        shape = (range_disc.ndof(sd), self.ndof(sd))
-        return sps.csc_array((data_IJ, (rows_I, cols_J)), shape=shape)
-
     def assemble_corotational_correction(
         self, sd: pg.Grid, rotation: np.ndarray
     ) -> sps.csc_array:
@@ -419,71 +389,6 @@ class MatPwLinears(MatPwPolynomials):
         # Return the corotational correction matrix
         return A_omega - omega_A
 
-    def assemble_mult_matrix(
-        self, sd: pg.Grid, mult_mat: np.ndarray, right_mult: bool
-    ) -> sps.csc_array:
-        """
-        Assembles and returns the multiplication matrix for the matrix-valued
-        piecewise constants.
-
-        Args:
-            sd (pg.Grid): The grid.
-            mult_mat (np.ndarray): The matrix to multiply with. It is assumed to be
-                a piecewise constant matrix.
-            right_mult (bool): If True, performs right multiplication. If False, left
-                multiplication.
-
-        Returns:
-            sps.csc_array: The multiplication matrix obtained from the discretization.
-        """
-        size = sd.dim**4 * (sd.dim + 1) * sd.num_cells
-        rows_I = np.empty(size, dtype=int)
-        cols_J = np.empty(size, dtype=int)
-        data_IJ = np.empty(size)
-        idx = 0
-
-        # Precompute the basis functions
-        basis = np.eye(sd.dim**2).reshape((sd.dim, sd.dim, -1))
-
-        if right_mult:
-            # right multiplication: A @ mult_mat
-            oper_str = "ijk,jp->ipk"
-        else:
-            # left multiplication: mult_mat @ A
-            oper_str = "ijk,pi->pjk"
-
-        for c in range(sd.num_cells):
-            # Get the local degrees of freedom for the cell ordered column wise
-            loc_dofs = self.local_dofs_of_cell(sd, c).reshape((-1, sd.dim + 1))
-
-            # Iterate over the node dofs
-            for node_dofs in loc_dofs.T:
-                mult_mat_loc = mult_mat[node_dofs].reshape((sd.dim, sd.dim))
-
-                # Compute the product A @ mult_mat (if right_mult is True)
-                # or mult_mat @ A (if right_mult is False)
-                prod = np.einsum(oper_str, basis, mult_mat_loc)
-
-                # Reshape the product to match the degrees of freedom
-                prod_at_dofs = np.array(
-                    [prod[:, :, i].ravel() for i in np.arange(sd.dim**2)]
-                )
-
-                # Save only the non-zeros values of the local matrix in the global
-                # structure
-                rows_loc, cols_loc = prod_at_dofs.nonzero()
-
-                # Save values of the local matrix in the global structure
-                loc_idx = slice(idx, idx + rows_loc.size)
-                rows_I[loc_idx] = node_dofs[cols_loc]
-                cols_J[loc_idx] = node_dofs[rows_loc]
-                data_IJ[loc_idx] = prod_at_dofs[rows_loc, cols_loc]
-                idx += rows_loc.size
-
-        # Construct the global matrices
-        shape = (self.ndof(sd), self.ndof(sd))
-        return sps.csc_array((data_IJ[:idx], (rows_I[:idx], cols_J[:idx])), shape=shape)
-
 
 class MatPwQuadratics(MatPwPolynomials):
     """
@@ -507,96 +412,3 @@ class MatPwQuadratics(MatPwPolynomials):
         """
         super().__init__(keyword)
         self.base_discr = pg.VecPwQuadratics(keyword)
-
-    def assemble_trace_matrix(self, sd: pg.Grid) -> sps.csc_array:
-        """
-        Assembles and returns the trace matrix for the matrix-valued
-        piecewise quadratics.
-
-        Args:
-            sd (pg.Grid): The grid.
-
-        Returns:
-            sps.csc_array: The trace matrix obtained from the discretization.
-        """
-        range_disc = pg.PwQuadratics()
-
-        size = range_disc.ndof_per_cell(sd) * sd.dim * sd.num_cells
-        rows_I = np.empty(size, dtype=int)
-        cols_J = np.empty(size, dtype=int)
-        data_IJ = np.ones(size)
-        idx = 0
-
-        if sd.dim == 1:
-            mask = np.arange(3)
-        elif sd.dim == 2:
-            mask = np.hstack((np.arange(6), np.arange(18, 24)))
-        elif sd.dim == 3:
-            mask = np.hstack((np.arange(10), np.arange(40, 50), np.arange(80, 90)))
-
-        for c in range(sd.num_cells):
-            loc_dofs = self.local_dofs_of_cell(sd, c)[mask]
-
-            ran_dofs = range_disc.local_dofs_of_cell(sd, c)
-            ran_dofs = np.tile(ran_dofs, sd.dim)
-
-            # Save values of the local matrix in the global structure
-            loc_idx = slice(idx, idx + loc_dofs.size)
-            rows_I[loc_idx] = ran_dofs
-            cols_J[loc_idx] = loc_dofs
-            idx += loc_dofs.size
-
-        # Construct the global matrices
-        shape = (range_disc.ndof(sd), self.ndof(sd))
-        return sps.csc_array((data_IJ, (rows_I, cols_J)), shape=shape)
-
-    def assemble_asym_matrix(self, sd: pg.Grid) -> sps.csc_array:
-        """
-        Assembles and returns the asymmetry matrix for the matrix-valued
-        piecewise quadratics.
-
-        Args:
-            sd (pg.Grid): The grid.
-
-        Returns:
-            sps.csc_array: The asymmetry matrix obtained from the discretization.
-        """
-        num_int_points = self.ndof_per_cell(sd) // (sd.dim**2)
-        size = num_int_points * (sd.dim * (sd.dim - 1)) * sd.num_cells
-        rows_I = np.empty(size, dtype=int)
-        cols_J = np.empty(size, dtype=int)
-        data_IJ = np.empty(size)
-        idx = 0
-
-        range_disc: pg.PwQuadratics | pg.VecPwQuadratics
-
-        if sd.dim == 2:
-            mask = np.arange(6, 18)
-            loc_data = np.repeat([-1, 1], num_int_points)
-            range_disc = pg.PwQuadratics()
-            rearrange = np.tile(np.arange(range_disc.ndof_per_cell(sd)), 2)
-
-        elif sd.dim == 3:
-            mask = np.hstack((np.arange(10, 40), np.arange(50, 80)))
-            loc_data = np.repeat([-1, 1, 1, -1, -1, 1], num_int_points)
-            range_disc = pg.VecPwQuadratics()
-
-            rearrange = np.arange(3 * num_int_points).reshape((3, -1))
-            rearrange = rearrange[[2, 1, 2, 0, 1, 0]].ravel()
-        else:
-            raise ValueError("The grid should be either two or three-dimensional")
-
-        for c in range(sd.num_cells):
-            loc_dofs = self.local_dofs_of_cell(sd, c)[mask]
-            ran_dofs = range_disc.local_dofs_of_cell(sd, c)[rearrange]
-
-            # Save values of the local matrix in the global structure
-            loc_idx = slice(idx, idx + loc_dofs.size)
-            rows_I[loc_idx] = ran_dofs
-            cols_J[loc_idx] = loc_dofs
-            data_IJ[loc_idx] = loc_data
-            idx += loc_dofs.size
-
-        # Construct the global matrices
-        shape = (range_disc.ndof(sd), self.ndof(sd))
-        return sps.csc_array((data_IJ, (rows_I, cols_J)), shape=shape)
