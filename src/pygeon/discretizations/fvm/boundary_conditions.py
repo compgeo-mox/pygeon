9f1c8fe1314fd825a63df8b0678969050e082fee
wmboon <wietseboon@gmail.com>
2026-05-21 10:00:00 +0200

    included check for double bcs

--- src/pygeon/discretizations/fvm/boundary_conditions.py
+++ src/pygeon/discretizations/fvm/boundary_conditions.py
@@ -1,3 +1,4 @@
+import warnings
 from typing import cast
 
 import numpy as np
@@ -11,7 +12,10 @@ class FiniteVolumeBC:
 
     def __init__(self, _: pg.Grid, data: dict, keyword: str) -> None:
         data[pp.PARAMETERS][keyword].update({"bc": self})
+
         self.weighted_dists: np.ndarray
+        self.primary_var = np.zeros_like(self.weighted_dists)
+        self.dual_var = np.zeros_like(self.weighted_dists)
 
     def _set_bcs(
         self,
@@ -29,7 +33,7 @@ class FiniteVolumeBC:
             indices = np.tile(indices, (self.dim_of_bc_vals, 1))
 
         assert input.shape == self.weighted_dists.shape, (
-            f"Input must be of shape ({self.dim_of_bc_vals}, num_faces)"
+            f"Input must be of shape {self.weighted_dists.shape}"
         )
 
         internal_var[indices] = input[indices]
@@ -39,29 +43,33 @@ class FiniteVolumeBC:
         else:
             self.weighted_dists[indices] = cast(np.ndarray, dist)[indices]
 
+        if np.any(np.logical_and(self.primary_var, self.dual_var)):
+            warnings.warn(
+                "Boundary conditions imposed on both the primary and dual variables may"
+                "lead to unexpected results"
+            )
+
 
 class ElasticityBC(FiniteVolumeBC):
     dim_of_bc_vals = pg.AMBIENT_DIM
 
-    def __init__(self, sd: pg.Grid, data: dict, keyword: str) -> None:
-        super().__init__(sd, data, keyword)
+    def __init__(self, sd: pg.Grid, data: dict, keyword: str = pg.UNITARY_DATA) -> None:
         self.weighted_dists = np.zeros((self.dim_of_bc_vals, sd.num_faces))
-        self.disp = np.zeros_like(self.weighted_dists)
-        self.trac = np.zeros_like(self.weighted_dists)
+        super().__init__(sd, data, keyword)
 
     def set_displacement_bcs(
         self,
         indices: np.ndarray | None = None,
         u_0: np.ndarray | None = None,
     ) -> None:
-        self._set_bcs(indices, u_0, self.disp, 0)
+        self._set_bcs(indices, u_0, self.primary_var, 0)
 
     def set_traction_bcs(
         self,
         indices: np.ndarray | None = None,
         sig_0: np.ndarray | None = None,
     ) -> None:
-        self._set_bcs(indices, sig_0, self.trac, np.inf)
+        self._set_bcs(indices, sig_0, self.dual_var, np.inf)
 
     def set_spring_bcs(
         self,
@@ -69,31 +77,29 @@ class ElasticityBC(FiniteVolumeBC):
         indices: np.ndarray | None = None,
         u_0: np.ndarray | None = None,
     ) -> None:
-        self._set_bcs(indices, u_0, self.disp, dists)
+        self._set_bcs(indices, u_0, self.primary_var, dists)
 
 
 class FlowBC(FiniteVolumeBC):
     dim_of_bc_vals = 1
 
-    def __init__(self, sd: pg.Grid, data: dict, keyword: str) -> None:
-        super().__init__(sd, data, keyword)
+    def __init__(self, sd: pg.Grid, data: dict, keyword: str = pg.UNITARY_DATA) -> None:
         self.weighted_dists = np.zeros(sd.num_faces)
-        self.pres = np.zeros_like(self.weighted_dists)
-        self.flux = np.zeros_like(self.weighted_dists)
+        super().__init__(sd, data, keyword)
 
     def set_pressure_bcs(
         self,
         indices: np.ndarray | None = None,
         p_0: np.ndarray | None = None,
     ) -> None:
-        self._set_bcs(indices, p_0, self.pres, 0)
+        self._set_bcs(indices, p_0, self.primary_var, 0)
 
     def set_flux_bcs(
         self,
         indices: np.ndarray | None = None,
         q_0: np.ndarray | None = None,
     ) -> None:
-        self._set_bcs(indices, q_0, self.flux, np.inf)
+        self._set_bcs(indices, q_0, self.dual_var, np.inf)
 
     def set_robin_bcs(
         self,
@@ -101,4 +107,4 @@ class FlowBC(FiniteVolumeBC):
         indices: np.ndarray | None = None,
         p_0: np.ndarray | None = None,
     ) -> None:
-        self._set_bcs(indices, p_0, self.pres, dists)
+        self._set_bcs(indices, p_0, self.primary_var, dists)
